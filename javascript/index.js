ee34a2b9f2b0168011560c3a64d6f736e6c46407
technokeen <prjbagde.124@gmail.com>
2021-08-19 13:31:56 +0530

    Changes to index.html file

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+
+    $('.autoplay')
+    .not(".slick-initialized")
+    .slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots:true,
+        prevArrow:false,
+        nextArrow:false,
+      });
+
+
